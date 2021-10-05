599d08c47e237638ca1d1ad1091ed8a37b786472
Melissa Persaud <melissapersaud17.mp@gmail.com>
2021-10-05 12:54:30 -0400

    compeleted zip code lab

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,70 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-
-function City(props) {
-  return (<div>This is the City component</div>);
+function City({city}) {
+  const {City,State} = city;
+  const {Lat,Long} = city;
+  const {EstimatedPopulation} = city;
+  const {TotalWages} = city;
+  return (
+    <div>
+      {City},{State}
+      <ul>
+        <li>State: {State}</li>
+        <li>Location: ({Lat},{Long})</li>
+        <li>Population(estimated): {EstimatedPopulation}</li>
+        <li>Total Wages: {TotalWages}</li>
+      </ul>
+    </div>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  //we want to listen to every change the user types or we listen when a user clicks a submit button
+  return (
+    <div>
+      ZipCode:
+      <input type="text" onChange={props.changeHandler} />
+    </div>
+  );
 }
 
+  
+//onChange is going to listen to whatever the user types
 
 class App extends Component {
+  state = {
+    zipCode: '',
+    cities: [],
+  };
+
+  zipChanged = (event) => {
+    //gets the zipcode
+    this.setState({ zipCode: event.target.value });
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${event.target.value}`)
+      .then((res) => res.json())
+      .then(data => {
+        this.setState({cities:data});
+        console.log(this.state.cities);
+      });
+  };
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField
+          zipCode={this.state.zipCode}
+          changeHandler={this.zipChanged}
+        />
+        
         <div>
-          <City />
-          <City />
+          {
+            this.state.cities.map(city => <City city={city}/>)
+          }
         </div>
       </div>
     );
@@ -29,3 +72,4 @@ class App extends Component {
 }
 
 export default App;
+//We don't want to hardcode the cities, we want it to be dynamic - we display the number of cities based on the API
